1b00af367ce8cd4ab414e799a98f62f4f086af0b
Matias De Santi <mdesanti90@gmail.com>
2013-08-16 16:12:26 -0300

    Added bullet. Player can fire when pressing ctrl

---
+++ tp1/src/Assets/scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public float detonationForce = 2000.0f;
+
+    float time = 0;
+
+    void Start()
+    {
+        rigidbody.AddForce(transform.up * detonationForce);
+    }
+
+    void FixedUpdate()
+    {
+        //VERY VERY IMPORTANT!! Destroy the bullet after a while
+        time += Time.deltaTime;
+        if (time > 1.0f) {
+            //GameObject.Destroy(gameObject);
+		}
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        Debug.Log("Hit a " + collision.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
+    }
+}
--- tp1/src/Assets/scripts/PlayerController.cs
+++ tp1/src/Assets/scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 // The RequireComponent attribute ensures a CharacterController is added to the
 // GameObject whenever a PlayerController is added
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
 	private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
+	public Transform bulletPrototype;
 
     void Start()
     {
@@ -16,9 +18,9 @@ public class PlayerController : MonoBehaviour
     }
 
     void FixedUpdate() {
-
+		loadGun();
     	//Ask the controller if we are in mid air, if not, then act
-		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         moveDirection.Normalize();
 		moveDirection *= speed;
 
@@ -27,5 +29,17 @@ public class PlayerController : MonoBehaviour
 
         //Just tell the controller where we want to move, it will handle collisions itself
 		controller.Move(moveDirection * Time.deltaTime);
+	}
+	
+	void loadGun() {
+		if (Input.GetKeyDown(KeyCode.LeftControl)) {
+            // Copy the prefab. the returned type of Instantiate is allways the same 
+            // type as the parameter, so it's safe to cast
+            Transform bullet = (Transform)GameObject.Instantiate(bulletPrototype);
+
+            //Set the bullet in it's initial position and rotation
+            bullet.position = transform.position*2;
+            bullet.transform.Rotate(0,0,0);
+        }
 	}
 }
