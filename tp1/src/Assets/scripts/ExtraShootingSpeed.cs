9b4a0c41d1ef5177ef5da6cd2a1ea52717744fbf
Matias De Santi <mdesanti90@gmail.com>
2013-08-25 14:53:10 -0300

    Working on extras

--- tp1/src/Assets/scripts/ExtraShootingSpeed.cs
+++ tp1/src/Assets/scripts/ExtraShootingSpeed.cs
@@ -6,7 +6,7 @@ public class ExtraShootingSpeed : MonoBehaviour
     void OnCollisionEnter(Collision collision) 
     {
 		if(collision.collider.gameObject.tag == "Player") {
-			Bullet.growthFactor *= (float)2;
+			Bullet.growthFactor *= (float)1.5;
 			GameObject.Destroy(gameObject);
 		}
     }		
