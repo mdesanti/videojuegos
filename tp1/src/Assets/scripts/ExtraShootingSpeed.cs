305203d6364b5d974c1153a26c11efad72a218d5
Esteban Pintos <epintos7@gmail.com>
2013-08-25 18:00:00 -0300

    Added extras score

--- tp1/src/Assets/scripts/ExtraShootingSpeed.cs
+++ tp1/src/Assets/scripts/ExtraShootingSpeed.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class ExtraShootingSpeed : MonoBehaviour
 {	
-
+	
+	public int score;
+	
     void OnCollisionEnter(Collision collision) 
     {
 		if(collision.collider.gameObject.tag == "Player") {
+			int playerScore = PlayerPrefs.GetInt("Score");
+			PlayerPrefs.SetInt("Score",playerScore + score);
 			Debug.Log("Hit a player!");
 			Bullet.growthFactor *= (float)1.5;
 			GameObject.Destroy(gameObject);
