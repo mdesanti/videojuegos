5be95de1a4648c28d22bb7b8c1f585f65be44990
Matias De Santi <mdesanti90@gmail.com>
2013-09-07 17:16:29 -0300

    Player now hits extras

---
+++ tp2/src/Assets/Scripts/ExtraSpeedController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExtraSpeedController : AbstractExtrasController
+{	
+	public override void perform(GameObject playerGameObject) {
+		//increase moving speed
+	}
+}
