d8a770945ac0688ff0447ce23d703bf670a92763
Matias De Santi <mdesanti90@gmail.com>
2013-09-08 15:45:57 -0300

    Autodestroys explotion after 5 seconds

---
+++ tp2/src/Assets/Scripts/ExplotionController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExplotionController : MonoBehaviour
+{	
+	void Start()
+    {
+		//autodestroy after 5 seconds
+        Destroy(gameObject, 5);
+    }
+}
