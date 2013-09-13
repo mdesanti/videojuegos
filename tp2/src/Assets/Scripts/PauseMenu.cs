aa272000a14f5e8128ae7c1314ab72da7577e7ed
Esteban Pintos <epintos7@gmail.com>
2013-09-13 13:50:33 -0300

    Added Menues

---
+++ tp2/src/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+
+using System;
+using UnityEngine;
+ 
+public class PauseMenu : MonoBehaviour
+{
+    bool paused = false;
+	public GUIStyle style;
+ 
+    void Update()
+    {
+       if(Input.GetKeyDown("escape"))
+         paused = togglePause();
+    }
+ 
+    void OnGUI(){
+       if(paused){
+       		GUI.Box(new Rect(Screen.width/2-325,30,650,500),"");	
+			GUI.Label (new Rect (Screen.width/2-165,65,100,70),"Pause Menu", style);	
+	       	if(GUI.Button (new Rect (Screen.width/2-70,200,150,50), "Continue")){
+	         	paused = togglePause();
+			}
+			if (GUI.Button (new Rect (Screen.width/2-70,290,150,50), "Restart Game")) {
+				paused = togglePause();
+				Application.LoadLevel("Level1");
+			}
+		
+       }
+    }
+ 
+	
+    bool togglePause(){
+       if(Time.timeScale == 0f){
+         Time.timeScale = 1f;
+         return(false);
+       }else{
+         Time.timeScale = 0f;
+         return(true);    
+       }
+    }
+}
---
+++ tp2/src/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+	
+
+	void OnGUI () {
+		GUI.Box(new Rect(Screen.width/2-325,30,650,500),"");
+		if (GUI.Button (new Rect (Screen.width/2-70,230,150,50), "Start")) {
+			Application.LoadLevel("Level1");
+		}
+	}
+}
