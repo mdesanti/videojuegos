5dad5229ed725bbc1e6b3c1f6195ef013f13fd95
Esteban Pintos <epintos7@gmail.com>
2013-10-18 00:07:46 -0300

    Modified player size and added basic door open script

---
+++ tp3/src/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour 
+{
+	public float smooth = (float)2.0;
+	public float DoorOpenAngle = (float)110.0;
+	public bool open = false;
+	
+
+	void Update () 
+	{
+		if(open == true)
+		{
+			transform.eulerAngles = new Vector3(90, transform.rotation.y, transform.rotation.z);
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+			//var target = Quaternion.Euler (transform.localRotation.x, transform.localRotation.y+DoorOpenAngle, transform.localRotation.z);
+			//transform.localRotation = Quaternion.Slerp(transform.localRotation, target,
+			//Time.deltaTime * smooth);
+			Debug.Log("opened");
+		}
+	
+	}
+
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			open = true;
+		}
+	}
+	
+}
+
