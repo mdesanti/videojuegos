4a349183ac9ac663fc9914d655c7fdf1eff07f43
Matias De Santi <mdesanti90@gmail.com>
2013-10-27 15:07:08 -0300

    Created time controller

---
+++ tp3/src/Assets/Scripts/TimeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour {
+	
+	public float availableTime = 0f;
+	public GUIStyle style;
+	
+	// Update is called once per frame
+	void Update () {
+		availableTime -= Time.deltaTime;
+		if(availableTime < 0) {
+			Debug.Log("Time is up!");
+			//loose!
+		}
+	}
+	
+	void OnGUI () {
+		GUI.Label (new Rect (Screen.width/2-65 + 150,140,100,50),"Time: " + availableTime, style);	
+	}
+}
