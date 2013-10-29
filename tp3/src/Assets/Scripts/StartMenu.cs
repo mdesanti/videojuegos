21f0e7e40e76a067488bdd1e954cc59e6a9d1035
Matias De Santi <mdesanti90@gmail.com>
2013-10-29 19:57:56 -0300

    Some fixes

--- tp3/src/Assets/Scripts/StartMenu.cs
+++ tp3/src/Assets/Scripts/StartMenu.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class StartMenu : MonoBehaviour {
 	
-	public AudioClip buttonClip;
 	
 	private string width = "400";
 	private string height = "400";
@@ -11,16 +10,19 @@ public class StartMenu : MonoBehaviour {
 	bool difficult = false;
 	
 	void OnGUI () {
-		GUI.Box(new Rect(Screen.width/2-325,30,650,500),"");
+		GUI.Box(new Rect(Screen.width/2-325,30,650,500), "");
 		
-		seed = GUI.TextField (new Rect(Screen.width/2-70,130,150,20), seed, 100);
-		width = GUI.TextField (new Rect(Screen.width/2-70,160,150,20), width, 100);
-		height = GUI.TextField (new Rect(Screen.width/2-70,190,150,20), height, 100);
-		difficult = GUI.Toggle(new Rect(Screen.width/2-70,220,150,20), difficult, "Dificil?");
+		GUI.Label(new Rect(Screen.width/2-10,220,150,20), "Semilla");
+		GUI.Label(new Rect(Screen.width/2-10,250,150,20), "Ancho");
+		GUI.Label(new Rect(Screen.width/2-10,280,150,20), "Alto");
+		
+		seed = GUI.TextField (new Rect(Screen.width/2-70,220,150,20), seed, 100);
+		width = GUI.TextField (new Rect(Screen.width/2-70,250,150,20), width, 100);
+		height = GUI.TextField (new Rect(Screen.width/2-70,280,150,20), height, 100);
+		difficult = GUI.Toggle(new Rect(Screen.width/2-70,310,150,20), difficult, "Dificil?");
 	
 		
-		if (GUI.Button (new Rect (Screen.width/2-70,250,150,50), "Start")) {
-			//AudioSource.PlayClipAtPoint(buttonClip , transform.position);
+		if (GUI.Button (new Rect (Screen.width/2-70,340,150,50), "Start")) {
 			PlayerPrefs.SetInt("Score",0);
 			PlayerPrefs.SetInt("Life", 100);
 			PlayerPrefs.SetInt("width", int.Parse(width));
